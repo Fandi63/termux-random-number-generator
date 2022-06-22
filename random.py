ed0a6bcbbd2222841732a5dc20093b0249c1b4f3
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-22 19:33:22 +0200

    Add files via upload

---
+++ random.py
@@ -0,0 +1,8 @@
+import os
+import random
+os.system("apt install figlet")
+os.system("clear")
+os.system("figlet random")
+print("tool by Fandi63")
+input("Press enter::>")
+print(random.randint(0,100000000000000000000000000000000000000000000000000000000))
